9bf5676912d4c592f65d30629ec43c9dbfa43258
choikj129 <ckj9829@naver.com>
2025-05-18 00:21:20 +0900

    feat : 404 ExceptionHandler 추가

--- module-api/src/main/java/com/flab/tableorder/exception/GlobalExceptionHandler.java
+++ module-api/src/main/java/com/flab/tableorder/exception/GlobalExceptionHandler.java
@@ -8,9 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ResponseDTO> handleNotFound(NoHandlerFoundException ex) {
+        ResponseDTO response = new ResponseDTO(404, "Requested resource not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
     @ExceptionHandler({EntityNotFoundException.class, StoreNotFoundException.class, MenuNotFoundException.class})
     public ResponseEntity<ResponseDTO> handleEntityNotFound(RuntimeException ex) {
         ResponseDTO responseData = new ResponseDTO(404, ex.getMessage());
