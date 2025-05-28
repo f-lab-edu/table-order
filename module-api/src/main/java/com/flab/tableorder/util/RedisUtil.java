92f7cdc4479acc3608831fc4bdc0f2a74702cea5
choikj129 <ckj9829@naver.com>
2025-05-28 22:57:17 +0900

    refactor : RedisUtil 들여쓰기 오타 수정

--- module-api/src/main/java/com/flab/tableorder/util/RedisUtil.java
+++ module-api/src/main/java/com/flab/tableorder/util/RedisUtil.java
@@ -2,6 +2,6 @@ package com.flab.tableorder.util;
 
 public class RedisUtil {
     public static String getRedisKey(String... keys) {
-            return String.join(":", keys);
-        }
+        return String.join(":", keys);
+    }
 }
