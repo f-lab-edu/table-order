7dd6868d5d8cc24e07f60b518ef54713f38de97a
choikj129 <ckj9829@naver.com>
2025-06-01 15:01:45 +0900

    feat : Stat document 추가

---
+++ module-api/src/main/java/com/flab/tableorder/domain/Stat.java
@@ -0,0 +1,19 @@
+package com.flab.tableorder.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "order_stats")
+@Getter @Setter @Builder
+public class Stat {
+    private ObjectId storeId;
+    private ObjectId menuId;
+    private ObjectId optionId;
+
+    private String date;
+    private int quantity;
+    private int price = 0;
+}
---
+++ module-api/src/main/java/com/flab/tableorder/domain/StatRepository.java
@@ -0,0 +1,47 @@
+package com.flab.tableorder.domain;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.BulkOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StatRepository {
+    private final MongoTemplate mongoTemplate;
+
+    public void IncrementOrderCount(List<Stat> stats) {
+        BulkOperations bulkOps = mongoTemplate.bulkOps(BulkOperations.BulkMode.ORDERED, Stat.class);
+
+        stats.forEach(stat -> {
+            Query query = new Query(new Criteria()
+                .andOperator(
+                    Criteria.where("storeId").is(stat.getStoreId()),
+                    Criteria.where("menuId").is(stat.getMenuId()),
+                    Criteria.where("optionId").is(stat.getOptionId()),
+                    Criteria.where("date").is(stat.getDate()),
+                    Criteria.where("price").is(stat.getPrice())));
+
+            Update update = new Update().inc("quantity", stat.getQuantity());
+            bulkOps.upsert(query, update);
+        });
+
+        bulkOps.execute();
+    }
+
+    public List<Stat> findAllOrderStats(ObjectId storeId, String date) {
+        Query query = new Query(new Criteria()
+            .andOperator(
+                Criteria.where("storeId").is(storeId),
+                Criteria.where("date").is(date)));
+
+        return mongoTemplate.find(query, Stat.class);
+    }
+}
